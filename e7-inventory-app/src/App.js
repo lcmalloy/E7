cd33e444650cc819a87bd6ec22582f9686b374ff
Lawrence Malloy <lcmalloy@gmail.com>
2023-02-28 19:01:58 -0800

    added horizontal banner to desktop layout, view hidden in mobile layout. view for vertical banner and nav section hidden in mobile view

--- e7-inventory-app/src/App.js
+++ e7-inventory-app/src/App.js
@@ -5,15 +5,16 @@ function App() {
   return (
     <div className="container primary3">
       <div className="header">
-        Header
         <div className="logo">logo</div>
+        <div className="horizontal-banner-main">E7 Banner</div>
+        <div className="nav-buttons">nav button</div>
       </div>
       <div className="nav">nav</div>
       <div className="hero">hero</div>
       <div className="equipment-selector">equipment</div>
       <div className="main">main</div>
       <div className="similar-item">display-similar-items</div>
-      <div className="banner">Banner</div>
+      <div className="vertical-banner-main">Banner</div>
       <div className="footer">footer</div>
     </div>
   );
