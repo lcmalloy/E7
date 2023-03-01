f5c11b781ee5f491f6578dceb800e2a60ea96fe3
Lawrence Malloy <lcmalloy@gmail.com>
2023-02-28 17:56:27 -0800

    added basic grid layout for mobile and desktop setup

--- e7-inventory-app/src/App.js
+++ e7-inventory-app/src/App.js
@@ -4,7 +4,15 @@ function App() {
 
   return (
     <div className="container">
-      Hello
+      <div className="logo">logo</div>
+      <div className="header">header</div>
+      <div className="nav">nav</div>
+      <div className="hero">hero</div>
+      <div className="equipment-selector">equipment</div>
+      <div className="main">main</div>
+      <div className="similar-item">display-similar-items</div>
+      <div className="banner">Banner</div>
+      <div className="footer">footer</div>
     </div>
   );
 }
