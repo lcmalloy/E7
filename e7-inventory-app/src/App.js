ecbe6a2f29003fb1954fe0ca1fd4c47891714863
Lawrence Malloy <lcmalloy@gmail.com>
2023-03-03 16:16:18 -0800

    added hp-bar to calculator component. created default values for calculator added css styling to calculator

--- e7-inventory-app/src/App.js
+++ e7-inventory-app/src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
-import Header from './components/header/Header.jsx';
-import Navbar from './components/Navbar/Navbar.jsx';
-import HpBar from './components/HpBar/HpBar.jsx'
+import Header from './components/Header/Header.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
+import Calculator from './components/Calculator/Calculator.jsx'
 
 
 const App = () => {
@@ -13,8 +13,7 @@ const App = () => {
       <div className="hero">hero</div>
       <div className="equipment-selector">equipment</div>
       <div className="main">
-        main
-      <HpBar />
+      <Calculator />
       </div>
       <div className="similar-item">display-similar-items</div>
       <div className="footer">footer</div>
