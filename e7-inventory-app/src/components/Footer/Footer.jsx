9e34d658e53b1feeffc906a44a4d0209681a3e96
Lawrence Malloy <lcmalloy@gmail.com>
2023-03-15 16:03:05 -0700

    removed unused dependencies

--- e7-inventory-app/src/components/Footer/Footer.jsx
+++ e7-inventory-app/src/components/Footer/Footer.jsx
@@ -5,9 +5,9 @@ import './footer.scss'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">E7 Management</a>
+      <a href="/#" className="footer__logo">E7 Management</a>
       <ul className="permalinks">
-        <li><a href="#">Top</a></li>
+        <li><a href="/#">Top</a></li>
         <li><a href="#about"> About</a></li>
         <li><a href="#tools"> Calculator</a></li>
       </ul>
